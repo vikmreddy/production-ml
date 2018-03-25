023c03bda8957fbae4079892b34b608dc8ba80d6
Vikram Reddy <vreddy@ischool.berkeley.edu>
2018-03-25 15:37:38 -0700

    dataset, features, and model instance testing

--- test/instances/test_mlapp.py
+++ test/instances/test_mlapp.py
@@ -9,3 +9,15 @@ def test_structured_data_instantiation():
                                'features_config.ini',
                                'model_config.ini')
     assert my_app
+
+def test_dataset_instance():
+    dataset = Dataset('/path/to/data')
+    assert dataset
+
+def test_features_instance():
+    features = Features('features_config.ini')
+    assert features
+
+def test_model_instance():
+    model = Model('model_config.ini')
+    assert model
